3d8f1b93c5bdb46f5313077cea27dc0dddad33bf
Awsuser12 <harithsaman@gmail.com>
2024-12-18 20:27:12 +0530

    Update redshift_client.py

--- redshift_client.py
+++ redshift_client.py
@@ -1,20 +1,25 @@
 import psycopg2
+import traceback
 from shared.config import Config
 
 def connect_to_redshift():
-    conn = psycopg2.connect(
-        dbname=Config.REDSHIFT_DB,
-        user=Config.REDSHIFT_USER,
-        password=Config.REDSHIFT_PASSWORD,
-        host=Config.REDSHIFT_HOST,
-        port=Config.REDSHIFT_PORT
-    )
-    return conn
-
+    try:
+        conn = psycopg2.connect(
+            dbname=Config.dev,
+            user=Config.awsuser,
+            password=Config.Admin$100,
+            host="healthsync-metrics.c9oyzhxa9puz.eu-north-1.redshift.amazonaws.com",
+            port=5439
+        )
+        return conn
+    except Exception as e:
+        print(f"Error connecting to Redshift: {e}")
+        raise  # Reraise the exception after logging
 
 def insert_aggregated_data(metrics):
-    conn = connect_to_redshift()
+    conn = None
     try:
+        conn = connect_to_redshift()
         with conn.cursor() as cur:
             for metric in metrics:
                 cur.execute(
@@ -28,6 +33,9 @@ def insert_aggregated_data(metrics):
         print("Metrics inserted successfully.")
     except Exception as e:
         print(f"Error inserting metrics: {e}")
-        conn.rollback()
+        print(traceback.format_exc())
+        if conn:
+            conn.rollback()  # Ensure rollback if something goes wrong
     finally:
-        conn.close()
+        if conn:
+            conn.close()  # Close connection after commit or rollback
